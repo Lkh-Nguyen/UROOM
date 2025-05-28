0859f6da235a71c88703f716bb85b40e8b7481be
nhatquyen1104 <thanhsang141203@gmail.com>
2025-05-28 17:45:36 +0700

    hotel setting

--- hotel_fe/src/pages/hotel_host/information/components/ViewAvatarHotel.jsx
+++ hotel_fe/src/pages/hotel_host/information/components/ViewAvatarHotel.jsx
@@ -1,48 +1,72 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  FaCog,
-  FaLock,
-  FaUser,
-  FaHistory,
-  FaHeart,
-  FaPen,
-} from "react-icons/fa";
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  Form,
-  Button,
-  Modal,
-} from "react-bootstrap";
+import { Modal, Card, Form, Button } from "react-bootstrap";
+import AuthActions from "../../../../redux/auth/actions";
+import { useAppSelector } from "../../../../redux/store";
+import { useDispatch } from "react-redux";
+import { showToast, ToastProvider } from "@components/ToastContainer";
+import ConfirmationModal from "@components/ConfirmationModal";
 
 function ViewAvatar() {
-  // State to control the avatar modal
+  const Auth = useAppSelector((state) => state.Auth.Auth);
   const [showAvatarModal, setShowAvatarModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(Auth?.image?.url);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [showAcceptModal, setShowAcceptModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-  // State lưu ảnh upload
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  // Functions to handle modal open/close
+  // Xử lý mở modal
   const handleOpenModal = () => setShowAvatarModal(true);
   const handleCloseModal = () => setShowAvatarModal(false);
 
-  // Xử lý chọn file ảnh
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setSelectedImage(imageUrl);
+      setSelectedFile(file);
     }
   };
+
+  const handleUploadFile = () => {
+    if (!selectedFile) {
+      showToast.warning("Please select a file to upload.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("avatar", selectedFile);
+    setLoading(true);
+    dispatch({
+      type: AuthActions.UPDATE_AVATAR,
+      payload: {
+        formData: formData,
+        onSuccess: (MsgYes) => {
+          setLoading(false);
+          showToast.success(MsgYes || "Avatar updated successfully!");
+        },
+        onFailed: (MsgNo) => {
+          showToast.warning(MsgNo || "Failed to update avatar.");
+        },
+        onError: (MsgNo) => {
+          showToast.warning(MsgNo || "Something went wrong!");
+        },
+      },
+    });
+  };
+
   return (
     <Card.Body>
       <h2 className="fw-bold mb-4">View Avatar</h2>
       <div className="text-center">
-      <img
-          src={selectedImage || "https://i.pinimg.com/736x/8f/1c/a2/8f1ca2029e2efceebd22fa05cca423d7.jpg"}
+        <img
+          src={
+            selectedImage != "" && selectedImage
+              ? selectedImage
+              : "https://i.pinimg.com/736x/8f/1c/a2/8f1ca2029e2efceebd22fa05cca423d7.jpg"
+          }
           className="rounded-circle mb-2"
           style={{ width: "150px", height: "150px", objectFit: "cover" }}
           alt="avatar"
@@ -57,13 +81,14 @@ function ViewAvatar() {
         </Button>
       </div>
       <p className="text-center text-muted">
-        Maximum file size in 1 MB
+        Maximum file size is 1 MB
         <br />
         Format JPEG, PNG, JPG, ...
       </p>
       <Form>
         <Form.Group controlId="formFile" className="mb-3 text-center">
           <Form.Control
+            name="avatar"
             type="file"
             className="d-inline-block w-auto"
             onChange={handleFileChange}
@@ -71,12 +96,38 @@ function ViewAvatar() {
           />
         </Form.Group>
         <div className="d-flex justify-content-end">
-          <Button variant="danger" className="me-2">
+          <Button
+            variant="danger"
+            className="me-2"
+            onClick={() => {
+              setShowUpdateModal(true);
+            }}
+          >
             CANCEL
           </Button>
-          <Button variant="primary">Upload</Button>
+          <Button
+            variant="primary"
+            disabled={loading} // disable khi loading
+            onClick={() => {
+              setShowAcceptModal(true);
+            }}
+          >
+            {loading ? (
+              <>
+                <span
+                  className="spinner-border spinner-border-sm me-2"
+                  role="status"
+                  aria-hidden="true"
+                ></span>
+                Uploading...
+              </>
+            ) : (
+              "Upload"
+            )}
+          </Button>
         </div>
       </Form>
+
       {/* Avatar Modal */}
       <Modal
         show={showAvatarModal}
@@ -89,10 +140,14 @@ function ViewAvatar() {
         </Modal.Header>
         <Modal.Body className="text-center p-4">
           <img
-            src={selectedImage || "https://i.pinimg.com/736x/8f/1c/a2/8f1ca2029e2efceebd22fa05cca423d7.jpg"}
+            src={
+              selectedImage != "" && selectedImage
+                ? selectedImage
+                : "https://i.pinimg.com/736x/8f/1c/a2/8f1ca2029e2efceebd22fa05cca423d7.jpg"
+            }
             alt="Customer avatar"
             className="img-fluid"
-            style={{ maxHeight: "70vh" }}
+            style={{ height: "480px", width: "480px" }}
           />
         </Modal.Body>
         <Modal.Footer>
@@ -101,6 +156,31 @@ function ViewAvatar() {
           </Button>
         </Modal.Footer>
       </Modal>
+      <ToastProvider />
+      {/* Update Confirmation Modal */}
+      <ConfirmationModal
+        show={showUpdateModal}
+        onHide={() => setShowUpdateModal(false)}
+        onConfirm={() => {
+          setSelectedImage(Auth.image.url);
+          setSelectedFile();
+        }}
+        title="Confirm Cancel"
+        message="Are you sure you want to reset this avatar ?"
+        confirmButtonText="Confirm"
+        type="warning"
+      />
+
+      {/* Accept Confirmation Modal */}
+      <ConfirmationModal
+        show={showAcceptModal}
+        onHide={() => setShowAcceptModal(false)}
+        onConfirm={handleUploadFile}
+        title="Confirm Update"
+        message="Are you sure you want to update this new avatar?"
+        confirmButtonText="Accept"
+        type="accept"
+      />
     </Card.Body>
   );
 }
--- hotel_fe/src/redux/auth/saga.js
+++ hotel_fe/src/redux/auth/saga.js
@@ -36,7 +36,7 @@ function* register() {
     const { data, onSuccess, onFailed, onError } = action.payload;
     console.log("Data: ", data);
     try {
-      const response = yield call(() => Factories.register_owner  (data));
+      const response = yield call(() => Factories.register_owner(data));
       if (response?.status === 200) {
         yield put({
           type: AuthActions.REGISTER_SUCCESS,
@@ -86,7 +86,7 @@ function* resend_verification() {
     const { data, onSuccess, onFailed, onError } = action.payload;
     try {
       const response = yield call(() => Factories.resend_verification(data));
-      
+
       if (response?.status === 200) {
         yield put({
           type: AuthActions.RESEND_VERIFICATION_SUCCESS,
@@ -105,12 +105,110 @@ function* resend_verification() {
     }
   });
 }
+function* update_profile() {
+  yield takeEvery(AuthActions.UPDATE_PROFILE, function* (action) {
+    const { data, onSuccess, onFailed, onError } = action.payload;
 
+    try {
+      const response = yield call(() => Factories.update_profile(data));
+
+      console.log("status: ", response?.status);
+      console.log("data: ", response?.data.Data);
+
+      if (response?.status === 200) {
+        const { user } = response.data.Data;
+
+        // Cập nhật vào redux store
+        yield put({
+          type: AuthActions.UPDATE_PROFILE_SUCCESS,
+          payload: { user },
+        });
+
+        onSuccess && onSuccess(user);
+      }
+    } catch (error) {
+      const status = error.response?.status;
+      const msg = error.response?.data?.MsgNo;
+
+      console.log("status: ", status);
+      console.log("msg: ", msg);
+
+      if (status >= 500) {
+        onError && onError(error);
+      } else {
+        onFailed && onFailed(msg);
+      }
+    }
+  });
+}
+function* change_password() {
+  yield takeEvery(AuthActions.CHANGE_PASSWORD, function* (action) {
+    const { data, onSuccess, onFailed, onError } = action.payload;
+
+    try {
+      const response = yield call(() => Factories.change_password(data));
+
+      if (response?.status === 200) {
+        yield put({
+          type: AuthActions.CHANGE_PASSWORD_SUCCESS,
+        });
+
+        onSuccess && onSuccess();
+      }
+    } catch (error) {
+      const status = error.response?.status;
+      const msg = error.response?.data?.MsgNo;
+
+      console.log("status: ", status);
+      console.log("msg: ", msg);
+
+      if (status >= 500) {
+        onError && onError(error);
+      } else {
+        onFailed && onFailed(msg);
+      }
+    }
+  });
+}
+function* update_avatar() {
+  yield takeEvery(AuthActions.UPDATE_AVATAR, function* (action) {
+    const { formData, onSuccess, onFailed, onError } = action.payload;
+
+    try {
+      const response = yield call(() => Factories.update_avatar(formData));
+
+      if (response?.status === 200) {
+        console.log("image: ", response?.data?.Data.image);
+        yield put({
+          type: AuthActions.UPDATE_AVATAR_SUCCESS,
+          payload: { image: response?.data?.Data.image },
+        });
+
+        onSuccess && onSuccess(response?.data?.Data.MsgYes);
+      }
+    } catch (error) {
+      const status = error.response?.status;
+      const msg = error.response?.data?.MsgNo;
+
+      console.log("status: ", status);
+      console.log("msg: ", msg);
+
+      if (status >= 500) {
+        onError && onError(error);
+      } else {
+        onFailed && onFailed(msg);
+      }
+    }
+  });
+}
 export default function* userSaga() {
   yield all([
     fork(login),
     fork(register),
     fork(verify_email),
     fork(resend_verification),
+    fork(update_avatar),
+    fork(update_profile),
+    fork(change_password),
   ]);
 }
